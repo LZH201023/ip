6adf12e494d950dcf191c7c2506b7e658c5763bb
Harry Lee <harrylee050114@gamil.com>
2026-01-20 16:12:02 +0800

    New commit

---
+++ src/main/java/Duck.java
@@ -0,0 +1,8 @@
+public class Duck {
+    public static void main(String[] args) {
+        String hLine = "-".repeat(50);
+        System.out.println(hLine + "\nHello! I'm Duck" +
+                            "\nWhat can I do for you?\n\n" + hLine +
+                            "\nBye. Hope to see you again soon!\n\n" + hLine);
+    }
+}
