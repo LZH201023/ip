25ee6652e8d15606c0dbbba8e9826515fd9e2bb2
Harry Lee <harrylee050114@gamil.com>
2026-01-21 18:16:49 +0800

    Add list functionality

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -1,33 +1,43 @@
 import java.util.Scanner;
 
 public class Duck {
+    private static String[] texts = new String[100];
+    private static int textsLen = 0;
 
-    public static boolean isBye(String s) {
+    private static boolean isBye(String s) {
         boolean b1 = s.charAt(0) == 'b' || s.charAt(0) == 'B';
         boolean b2 = s.charAt(1) == 'y' || s.charAt(1) == 'Y';
         boolean b3 = s.charAt(2) == 'e' || s.charAt(2) == 'E';
-        return b1 && b2 && b3;
+        return (s.length() == 3) && b1 && b2 && b3;
     }
+
     public static void main(String[] args) {
         String hLine = "-".repeat(50);
         Scanner sc = new Scanner(System.in);
         System.out.println(hLine + "\nHello! I'm Duck" +
-                            "\nWhat can I do for you?\n" + hLine + "\n");
+                            "\nWhat can I do for you?\n" + hLine);
 
         while (true) {
             String next = sc.nextLine();
             String s = next.stripLeading();
-            if (s.length() < 3) {
-                System.out.println(hLine + "\n" + next + "\n" + hLine);
-            } else if (isBye(s)){
+            if (s.length() < 3) { // Add item
+                texts[textsLen++] = next;
+                System.out.println(hLine + "\nadded: " + next + "\n" + hLine);
+            } else if (s.equals("list")) {
+                System.out.println(hLine);
+                for (int i = 1; i <= textsLen; i++) {
+                    System.out.println("\n" + i + ". " + texts[i - 1]);
+                }
+                System.out.println("\n" + hLine);
+            } else if (isBye(s)) {
                 System.out.println(hLine + "\n" +
                         "Bye. Hope to see you again soon!\n" +
                         hLine);
                 break;
             } else {
-                System.out.println(hLine + "\n" + next + "\n" + hLine);
+                texts[textsLen++] = next;
+                System.out.println(hLine + "\nadded: " + next + "\n" + hLine);
             }
         }
-//                            "\nBye. Hope to see you again soon!\n\n" + hLine);
     }
 }
