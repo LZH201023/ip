71215c49d7b4e3d743592dadcd9157991b475773
Harry Lee <harrylee050114@gamil.com>
2026-02-20 18:41:11 +0800

    Add more testing

--- src/test/java/duck/ParserTest.java
+++ src/test/java/duck/ParserTest.java
@@ -25,4 +25,32 @@ class ParserTest {
         }
     }
 
+    /**
+     * Tests that an invalid todo command format
+     * throws a {@link DuckException} with the correct error message.
+     */
+    @Test
+    void testParse_noDescriptionForTodoTask_throwDuckException() {
+        try {
+            Parser.parse("todo");
+            fail();
+        } catch (DuckException e) {
+            assertEquals("Missing todo description!", e.getMessage());
+        }
+    }
+
+    /**
+     * Tests that an invalid event command format
+     * throws a {@link DuckException} with the correct error message.
+     */
+    @Test
+    void testParse_extraInputArgument_throwDuckException() {
+        try {
+            Parser.parse("event ip /from 2026-01-15 /from 2020-02-02 /to 2027-01-01");
+            fail();
+        } catch (DuckException e) {
+            assertEquals("Wrong command format.", e.getMessage());
+        }
+    }
+
 }
